768b757f4cc1f8ca0ddccbed488305a8b9478384
Danijela Kralus <kralusdanijela@gmail.com>
2022-11-17 21:56:52 +0100

    bug fix

--- ISA_FE/src/app/modules/model/bloodbank.model.ts
+++ ISA_FE/src/app/modules/model/bloodbank.model.ts
@@ -1,33 +1,33 @@
-import { UserResponse, WholeUserResponse } from "./user.model";
+import { UserResponse } from "./user.model";
 
 export class BloodBankResponse {
     name: string = '';
-    id:number=0;
     description: string = '';
     averageRate: number = 0;
+    id: number = 0;
     address = {
         country: "",
         city: "",
         street: "",
         number: ""
     };
-    administrators: WholeUserResponse[] = [];
+    administrators: UserResponse[] = [];
     workTime = {
         startTime: new Date(),
         endTime: new Date()
-    };
+    }
     
 
 
     public constructor(obj?: any) {
         if (obj) {
             this.name = obj.name;
-            this.description = obj.description;
+            this.description = obj.descripton;
             this.averageRate = obj.averageRate;
             this.address = obj.address;  
             this.administrators = obj.administrators;  
-            this.workTime = obj.workTime;     
-            this.id = obj.id;    
+            this.workTime = obj.workTime; 
+            this.id = obj.id;       
         }
     }
 }
